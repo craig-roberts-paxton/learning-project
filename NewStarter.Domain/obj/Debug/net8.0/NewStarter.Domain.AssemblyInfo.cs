6c5fb8a3bee4727b456441a7403b995d86573da3
craig-roberts-paxton <craig.roberts@paxton-access.co.uk>
2024-02-26 09:12:59 +0000

    Rebasing

--- NewStarter.Domain/obj/Debug/net8.0/NewStarter.Domain.AssemblyInfo.cs
+++ NewStarter.Domain/obj/Debug/net8.0/NewStarter.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewStarter.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6244fa4911277977b9424efcbf1f46f51622f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d8ca0860f269d14010359a120c672aff6eba27")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewStarter.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewStarter.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
